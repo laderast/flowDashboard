02219b88bcbb7e0ab89812e5bd26f4b6e4a703cf
laderast <roshioshi@gmail.com>
2016-11-10 17:37:20 -0800

    Gating Module and QC module almost completely functional.

--- R/gatingModules.R
+++ R/gatingModules.R
@@ -135,23 +135,29 @@ getPopulationsAndZscores <- function(gateSet, pipelineFile="panel1"){
 #' @export
 #'
 #' @examples
-gatingModuleUI <- function(id, label = "qcViolin", sortChoices){
-  tagList(
+gatingModuleUI <- function(id, label = "qcViolin", sortConditions, subsetCondition, annotation){
+
+
+    ns <- NS(id)
+    subsetChoices <- unique(as.character(annotation[[subsetCondition]]))
+
+    tagList(
     h4("Filter Samples"),
     # checkboxGroupInput("panelDisplayPop", label="Show Panel",
     #                    choices=c("1", "3"), selected=c("1","3")),
-    selectInput('colSortPop', "Order Samples", choices=
-                  c("runDate"="runDate","Panel"="Panel","Sample"="patientID"),
-                selected="Panel"),
+    selectInput(ns("subset"), paste0("Select ", subsetCondition, " to display"), choices=subsetChoices,
+                selected = subsetChoices[1]),
+    selectInput('colSortPop', "Order Samples", choices= sortConditions,
+                selected=sortConditions[1]),
     absolutePanel(id="heatmap", h4("Population Heatmap (Click on box to see provenance)"),
                   ggvisOutput("populationHeatmap"), top=250, left=0),
-    absolutePanel(id="scheme",imageOutput(ns("pipelineHierarchy")), top=250, left=650),
+    # absolutePanel(id="scheme",imageOutput(ns("pipelineHierarchy")), top=250, left=650),
     absolutePanel(id="gating",draggable=TRUE,top=0, left=0,
-                  fixed=FALSE,
-                  style="opacity: 0.8; background-color: white",
-                  height=200,width="auto",
-                  h4("Gating Scheme (draggable)"),
-                  imageOutput(ns("gating"))),
+                   fixed=FALSE,
+                   style="opacity: 0.8; background-color: white",
+                   height=200,width="auto",
+                   h4("Gating Scheme (draggable)"),
+                   imageOutput(ns("gating"))),
     br(), br(), br(), br(), br()
     )
 
@@ -172,43 +178,68 @@ gatingModuleUI <- function(id, label = "qcViolin", sortChoices){
 #'
 #' @examples
 gatingModuleOutput <- function(input, output, session,
-                               imageDir, popTable, displayNodes){
-  PopTable <- reactive({
-    if(is.null(input$panelDisplayPop)){
-      panelDisplayPop <- c("1","3")
-    }
-    else{
-      panelDisplayPop <- input$panelDisplayPop
-    }
+                               imageDir, popTable, displayNodes, annotation, plotObj){
 
-    if(is.null(input$colSortPop)){
-      colSortPop <- "patientID"
-    }
-    else{
-      colSortPop <- input$colSortPop
-    }
+  annotateSelect <- reactive({
+    subsetVar <- input$subset
+    print(subsetVar)
 
-    out <- popTable %>%
-      #inner_join(y=SampleTable, by=c("name"="notation")) %>%
-      filter(panelVersion %in% panelDisplayPop) %>%
-      #filter(as.character(Population) %in% displayNodes)
-      arrange_(colSortPop)
-    #mutate(idVar = paste0(name,"_",Population),
-    #       percentPop =(Count/ParentCount)*100)
-    out$Population <- as.character(out$Population)
-    out
+    annotate2 <- annotation[patientID %in% subsetVar] #%>% dplyr::filter(patientID %in% subsetVar)
+    #annotate2 <- data.table(annotate2)
+    #setkey(annotate2, FCSFiles)
+    annotate2
   })
+
+
+  # displayNodes <- reactive({
+  #   if(is.null(displayNodes)){
+  #     displayNodes <- levels(popTable[[Population]])
+  #   }else{
+  #     displayNodes <- displayNodes
+  #   }
+  #   displayNodes
+  # })
+
+
+#   PopTable <- reactive({
+# #    if(is.null(input$panelDisplayPop)){
+#  #     panelDisplayPop <- c("1","3")
+# #    }
+#  #   else{
+#       #subsetVar <- input$subset
+#   #  }
+#
+#     if(is.null(input$colSortPop)){
+#       colSortPop <- "patientID"
+#     }
+#     else{
+#       colSortPop <- input$colSortPop
+#     }
+#
+#
+#     out <- popTable[annotateSelect()] #%>%
+#       #inner_join(y=SampleTable, by=c("name"="notation")) %>%
+#       #dplyr::filter(patientID %in% subsetVar) #%>%
+#       #filter(as.character(Population) %in% displayNodes)
+#       #arrange_(colSortPop)
+#     #mutate(idVar = paste0(name,"_",Population),
+#     #       percentPop =(Count/ParentCount)*100)
+#     #out$Population <- as.character(out$Population)
+#     print(head(out))
+#     out
+#   })
 
   popTooltip <- function(x){
     if(is.null(x)) return(NULL)
     #out <- paste0("<img src='data/images/", x$idVar, ".png'></img>")
-    plots[["gating"]] <- paste0(imageDir, x$idVar, ".png")
+    plotObj[["gating"]] <- paste0(imageDir, x$idVar, ".png")
     #print(out)
     #out
+    print(x$idVar)
   }
 
   pngGraph <- reactive({
-    return(plots[["gating"]])
+    return(plotObj[["gating"]])
   })
 
   output$gating <- renderImage({
@@ -220,25 +251,40 @@ gatingModuleOutput <- function(input, output, session,
 
   populationHeatmap <- reactive({
 
-    if(length(Samples())==0){
-      domX <- domXspare
-    }else{
-      domX <- Samples()
-    }
+    indata <- na.omit(popTable[annotateSelect()])
+    print(indata)
+    #print(annotateSelect())
+    populationHeatmapPlot(indata, displayNodes)
 
+    #if(length(Samples())==0){
+    #  domX <- domXspare
+    #}else{
+    #  domX <- Samples()
+    #}
+  })
 
-    noSamples <- length(unique(PopTable()$name))
+  populationHeatmapPlot <- function(data, displayNodes){
+
+    #print(displayNodes)
+    domY <- unique(as.character(data[["Population"]]))
+    displayNodes <- displayNodes[displayNodes %in% domY]
+    print(displayNodes)
+
+    noSamples <- length(unique(data$name))
     #noMarkers <- length(unique(PopTable()$Population))
     noMarkers <- length(displayNodes)
     #domX <- Samples()
-    popNotation <- as.character(unique(PopTable()$notation))
-    domX <- domX[domX %in% popNotation]
+
+    domX <- unique(as.character(data[["notation"]]))
+    print(domX)
+    #popNotation <- as.character(unique(data$notation))
+    #domX <- domX[domX %in% popNotation]
     #print(domX)
 
     Blue <- colorRampPalette(c("darkblue","lightblue"))
     Orange <- colorRampPalette(c("orange","darkorange3"))
 
-    levs <- sort(unique(round(PopTable()$zscore)))
+    levs <- sort(unique(round(data$zscore)))
 
     #print(levs)
 
@@ -250,9 +296,9 @@ gatingModuleOutput <- function(input, output, session,
 
     #pal <- c(Blue(3), "#E5E5E5", Orange(6))
 
-    PopTable() %>%
+    data[Population %in% displayNodes] %>%
       #filter(as.character(Population) %in% displayNodes) %>%
-      ggvis(x=~name,y= ~Population, fill=~factor(round(zscore))) %>%
+      ggvis(x=~notation,y= ~Population, fill=~factor(round(zscore))) %>%
       #ggvis(x=~name,y= ~Population, fill=~factor(round(Count))) %>%
       layer_rects(height = band(), width = band(), key:=~idVar) %>%
       scale_ordinal('fill',range = pal) %>%
@@ -262,12 +308,19 @@ gatingModuleOutput <- function(input, output, session,
       add_tooltip(popTooltip,on="click") %>%
       #add_tooltip(popInfoTooltip, on="hover") %>%
       scale_nominal("y", padding = 0, points = FALSE, domain = displayNodes) %>%
-      #scale_nominal("x", padding = 0, points = FALSE, domain = domX) %>%
+      scale_nominal("x", padding = 0, points = FALSE, domain = domX) %>%
       layer_text(text:=~signif(percentPop,digits=2), stroke:="darkgrey", align:="left",
                  baseline:="top", dx := 5, dy:=5) %>%
       set_options(width= 60 * (noSamples), height= 60 * (noMarkers))
-  })
+  }
 
   populationHeatmap %>% bind_shiny("populationHeatmap")
+
+}
+
+padMissingValues <- function(popTable){
+  populations <- unique(as.character(popTable[["Population"]]))
+  samples <- unique(as.character(popTable[["notation"]]))
+  expand.grid(populations, samples)
 
 }
