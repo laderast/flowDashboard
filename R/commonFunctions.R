1ec05f889150d0720b02aaf906d6480efaf92e18
laderast <roshioshi@gmail.com>
2016-12-14 13:05:35 -0800

    update of doc

--- R/commonFunctions.R
+++ R/commonFunctions.R
@@ -10,6 +10,50 @@
 #' @export
 #'
 #' @examples
+plotAllPopulationsOld <- function(gateSet, pipelineFile = "panel1",
+                               imagePath= "images/", delimiter="+"){
+  if(!dir.exists(imagePath)){
+    dir.create(imagePath)
+  }
+  require(flowWorkspace)
+
+  #for each node in the gatingTemplate, plot complete path
+  for(i in 1:length(gateSet)){
+    #samp <- gateSet[[i]]
+    sampName <- sampleNames(gateSet)[i]
+    print(sampName)
+    for(node in getNodes(gateSet[[i]], path="full")){
+      print(node)
+      if(node != "root"){
+        outnodes <- strsplit(x = node, split="/")[[1]]
+        outnodes <- setdiff(outnodes, c(""))
+        outPop <- outnodes[length(outnodes)]
+        #outnodes <- unlist(outnodes)
+        fileId <- paste0(imagePath, sampName, delimiter, outPop, delimiter, pipelineFile, ".png")
+        png(fileId, width=150*length(outnodes), height=150)
+        try(plotGate(gateSet[[i]], y=outnodes, default.y="Cell_length",checkName=FALSE,
+                     marker.only=TRUE, raw.scale=FALSE,
+                     gpar = list(nrow=1, ncol=length(outnodes))))
+        dev.off()
+      }
+    }
+  }
+}
+
+
+
+#' Plot all Populations
+#'
+#' Given a gatingSet, plot the provenance for each sample and each population.
+#'
+#'
+#' @param gateSet - a gatingSet with attached populations
+#' @param imagePath - directory to write population images
+#'
+#' @return nothing. Side effect is images written to the imagePath.
+#' @export
+#'
+#' @examples
 plotAllPopulations <- function(gateSet, nodeList, pipelineFile = "panel1",
                                imagePath= "images/", cytof=TRUE, delimiter="+", scaling=FALSE){
   if(!dir.exists(imagePath)){
