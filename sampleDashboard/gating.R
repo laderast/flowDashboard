25ba50c2795863355a08b05e0c498e39dc017865
laderast <roshioshi@gmail.com>
2017-07-28 09:40:51 -0700

    fixed bug

--- sampleDashboard/gating.R
+++ sampleDashboard/gating.R
@@ -45,6 +45,7 @@ output$gating2 <- renderImage({
 
 outDat <- reactive({
   popSubset <- input$ps
+  ##need to add sort by levels
   outDat <- GO$popTable[annotationGO4(), on=GO$mapVar][Population %in% popSubsets[[popSubset]]]
   outDat
 })
@@ -65,18 +66,34 @@ output$clickTip <- renderUI({
   if(is.null(click$x)){
     return(NULL)
   }
-  #print(hover)
-  #print(click$x)
-  #print(click)
 
-  #This doesn't work - need to write new scaling function for geom_tile()
-  #point <- nearPoints(outDatSpread(), click, threshold = 20, maxpoints = 1, xvar="name", yvar="Population")
-  #print(point)
   click$x <- click$x - 0.5
   click$y <- click$y - 0.5
 
   point <- findPointsGeomTile(click, data=outDat(), spreaddata = outDatSpread())
 
+  print(point)
+
+  #point <- outDat()[floor(click$x),]
+  outClick <- paste0(imageDir, point$idVar, ".png")
+  plotObj2[["gating"]] <- outClick
+  #print(outClick)
+  return(NULL)
+})
+
+##need to add hovertips
+output$hoverTip <- renderUI({
+
+  click <- input$clickGate
+
+  if(is.null(click$x)){
+    return(NULL)
+  }
+
+  click$x <- click$x - 0.5
+  click$y <- click$y - 0.5
+
+  point <- findPointsGeomTile(click, data=outDat(), spreaddata = outDatSpread())
 
   print(point)
 
